1882bdf4ae87639f70d2808a6f5f5b7d5bfcab31
ritesh-chafer <riteshprasadsingh8@gmail.com>
2020-07-28 15:52:36 +0530

    :

--- main.py
+++ main.py
@@ -10,13 +10,15 @@ def index():
 def about():
     return render_template('about.html')
 
+@app.route('/post')
+def post():
+    return render_template('post.html')
+
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
 
-@app.route('/post')
-def post():
-    return render_template('post.html')
+
 
 
 
